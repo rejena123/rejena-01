3fef31619dc5afd4a115cf19dbf61406870697b8
rejena123 <36025270+rejena123@users.noreply.github.com>
2018-04-11 16:40:28 +0800

    Update menu-2.php

--- menu-2.php
+++ menu-2.php
@@ -15,19 +15,19 @@ body{
 </head>
 <body>
 <h3>My Information</h3>
-<p><a href="rejena.html" target="content">Rejena Grace Pitogo</a></p>
+<p><a href="rejena.php" target="content">Rejena Grace Pitogo</a></p>
 <h3>CUTETOONS</h3>
-<p><a href="stephanie.html" target="content">Stephanie Manloloyo</a></p>
-<p><a href="arlene.html" target="content">Arlene Alfeche</a></p>
-<p><a href="alenie.html" target="content">Alenie Sawal</a></p>
-<p><a href="divina.html" target="content">Divina De Jesus</a></p>
-<p><a href="rosel.html" target="content">Rosel Aliganga</a></p>
+<p><a href="stephanie.php" target="content">Stephanie Manloloyo</a></p>
+<p><a href="arlene.php" target="content">Arlene Alfeche</a></p>
+<p><a href="alenie.php" target="content">Alenie Sawal</a></p>
+<p><a href="divina.php" target="content">Divina De Jesus</a></p>
+<p><a href="rosel.php" target="content">Rosel Aliganga</a></p>
 <h3>My Sister</h3>
-<p><a href="lhem.html" target="content">Lhem Jane Malazarte</a></p>
-<p><a href="love.html" target="content">Maria Lovena Gamol</a></p>
+<p><a href="lhem.php" target="content">Lhem Jane Malazarte</a></p>
+<p><a href="love.php" target="content">Maria Lovena Gamol</a></p>
 <h3>My High School Friends</h3>
-<p><a href="daniela.html" target="content">Daniela Jane Velez</a></p>
-<p><a href="jennievive.html" target="content">Jennievive Bacus</a></p>
+<p><a href="daniela.php" target="content">Daniela Jane Velez</a></p>
+<p><a href="jennievive.php" target="content">Jennievive Bacus</a></p>
 
 </body>
 </html>
