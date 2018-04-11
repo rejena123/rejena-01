9688fc0fb975c047c95fe38a7156f5bf077da86f
rejena123 <36025270+rejena123@users.noreply.github.com>
2018-04-11 16:37:41 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -17,16 +17,16 @@ a{
 </head>
 <body>
 <h3>Informations</h3>
-<p><a href="rejena.html" target="content">Rejena Pitogo</a>
-|<a href="stephanie.html" target="content">Stephanie Manloloyo</a>
-|<a href="arlene.html" target="content">Arlene Alfeche</a>
-|<a href="alenie.html" target="content">Alenie Sawal</a>
-|<a href="divina.html" target="content">Divina De Jesus</a>
-|<a href="rosel.html" target="content">Rosel Aliganga</a>
-|<a href="lhem.html" target="content">Lhem Jane Malazarte</a>
-|<a href="love.html" target="content">Maria Lovena Gamol</a>
-|<a href="daniela.html" target="content">Daniela Jane Velez</a>
-|<a href="jennievive.html" target="content">Jennievive Bacus</a></p>
+<p><a href="rejena.php" target="content">Rejena Pitogo</a>
+|<a href="stephanie.php" target="content">Stephanie Manloloyo</a>
+|<a href="arlene.php" target="content">Arlene Alfeche</a>
+|<a href="alenie.php" target="content">Alenie Sawal</a>
+|<a href="divina.php" target="content">Divina De Jesus</a>
+|<a href="rosel.php" target="content">Rosel Aliganga</a>
+|<a href="lhem.php" target="content">Lhem Jane Malazarte</a>
+|<a href="love.php" target="content">Maria Lovena Gamol</a>
+|<a href="daniela.php" target="content">Daniela Jane Velez</a>
+|<a href="jennievive.php" target="content">Jennievive Bacus</a></p>
 
 </body>
 </html>
